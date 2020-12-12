48d84a80ba83e497eb4aa1039afa157a11c8b3f7
Audrey King <audreymking1@gmail.com>
2020-12-12 17:32:18 -0600

    Added moment to display current time

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
 $(document).ready(function() {
 
-    
-    // GIVEN I am using a daily planner to create a schedule
-    // WHEN I open the planner
-    // THEN the current day is displayed at the top of the calendar
+    //shows the date-- attached to currentDay ID
+    var currentDate = moment().format("dddd, MMMM Do");
+    $("#currentDay").text(currentDate);
 
     
-    var displayTime = $("#currentDay");
-    var DateTime = luxon.DateTime;
-
-    var currentDate = JSON.stringify(DateTime.DATETIME_FULL);
-    //DateTime.local()
+    // WHEN I scroll down
+    // THEN I am presented with time blocks for standard business hours
+    //8 blocks of time
     
-    console.log(currentDate)
-    displayTime.text(currentDate);
+    //variable that takes the current time from moment
+    var currentTime = moment()hours();
+
+    //variable that is attached to workTime ID
+    var textBoxes = $(".workTime");
     
-});    
-
     
+    
+    // WHEN I view the time blocks for that day
+    // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+    
+    // WHEN I click into a time block
+    // THEN I can enter an event
+    
+    // WHEN I click the save button for that time block
+    // THEN the text for that event is saved in local storage
+    
+    // WHEN I refresh the page
+    // THEN the saved events persist
 
 
-
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-//8 blocks of time
-var textArea1 = 
-
-
-
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-
-// WHEN I click into a time block
-// THEN I can enter an event
-
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-
-// WHEN I refresh the page
-// THEN the saved events persist
-
-
+});
