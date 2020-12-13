17a1b39a459400fe3234a1da7fc1630103bdf036
Audrey King <audreymking1@gmail.com>
2020-12-12 18:56:58 -0600

    added loop to change color of text boxes, added local storage and ability to save

--- script.js
+++ script.js
@@ -10,24 +10,66 @@ $(document).ready(function() {
     //8 blocks of time
     
     //variable that takes the current time from moment
-    var currentTime = moment()hours();
+    var currentTime = 11
 
     //variable that is attached to workTime ID
     var textBoxes = $(".workTime");
-    
-    
+
+    //loop to go through all text boxes- to color the textboxes according to time
+    for (var i = 0; i < textBoxes.length; i++) {
+        //variable to add workID to each textBoxes in the loop
+        var workHour = $(textBoxes[i]).attr('workID');
+        if (currentTime > workHour) {
+            $(textBoxes[i]).addClass("past"); //past- turns textbox gray
+        } else if (currentTime < workHour) {
+            $(textBoxes[i]).addClass("future"); //future- turns textbox green
+        } else {
+            $(textBoxes[i]).addClass("present"); //present- turns textbox red
+        }
+    }
     
     // WHEN I view the time blocks for that day
     // THEN each time block is color-coded to indicate whether it is in the past, present, or future
+    
+    //local storage
+    //attaches a click function when save-btn is clicked
+    $(".save-btn").on("click", function(event){
+        //variable for when save-btn is clicked- selects one element up and adds to workID-- for localStorage key
+        var hourInput = $(event.target).parent().attr("workID");
+        //variable for when save-btn is clicked- accesses the textarea box, val returns what user inputed into textbox-- for localStorage value
+        var textInput = $(event.target).siblings("textarea").val();
+        
+        //takes hourInput and textInput and stores it into localStorage
+        localStorage.setItem(hourInput, textInput);
+    })
     
     // WHEN I click into a time block
     // THEN I can enter an event
     
     // WHEN I click the save button for that time block
     // THEN the text for that event is saved in local storage
+
+    //accesses workTime with workID- takes value of localStorage to display even if page has been refreshed
+    $(".workTime[workID='9'] textarea").val(localStorage.getItem("9"));
+
+    $(".workTime[workID='10'] textarea").val(localStorage.getItem("10"));
+
+    $(".workTime[workID='11'] textarea").val(localStorage.getItem("11"));
+
+    $(".workTime[workID='12'] textarea").val(localStorage.getItem("12"));
+
+    $(".workTime[workID='13'] textarea").val(localStorage.getItem("13"));
+
+    $(".workTime[workID='14'] textarea").val(localStorage.getItem("14"));
+
+    $(".workTime[workID='15'] textarea").val(localStorage.getItem("15"));
+
+    $(".workTime[workID='16'] textarea").val(localStorage.getItem("16"));
+
+    $(".workTime[workID='17'] textarea").val(localStorage.getItem("17"));
+  });    
+  
     
     // WHEN I refresh the page
     // THEN the saved events persist
 
-
-});
